9603d7cea2109e664a8c57a5e95e1916c581b925
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-02-06 20:47:12 +0100

    Changes in JS part

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 const tabs = document.querySelectorAll(".tab");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    TabManager.switchTab(tab.dataset.tab);
-  });
-});
-
-document.querySelectorAll(".tab").forEach((tab) => {
-  tab.classList.remove("active");
-});
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+tabs.forEach(function (tab) {
+  tab.addEventListener("click", function () {
+    tabs.forEach(function (tbs) {
+      tbs.classList.remove("active");
+    });
     tab.classList.add("active");
   });
 });
-
-document.querySelectorAll(".tab-content").forEach((content) => {
-  content.classList.remove("active");
-});
-const tabName = tab.dataset.tab;
-document.getElementById(`${tabName}-section`).classList.add("active");
