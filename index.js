1c18d52365620e0e4c8c3497ddbef64e6b5da05b
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-02-06 23:31:22 +0100

    Changes in JS

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ tabs.forEach(function (tab) {
       tbs.classList.remove("active");
     });
     tab.classList.add("active");
+
+    const content = document.querySelectorAll(".tab-content");
+    content.forEach((c) => c.classList.remove("active"));
+
+    const name = tab.dataset.tab;
+    const id = name + "-section";
+
+    document.getElementById(id).classList.add("active");
   });
 });
