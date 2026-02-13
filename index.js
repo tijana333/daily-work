3c526419eb7fa693d1b2b8253d9889bd1c427cc6
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-02-13 11:32:27 +0100

    JS part for Today section

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ tabs.forEach(function (tab) {
       tbs.classList.remove("active");
     });
     tab.classList.add("active");
-    // volela bih objasnjenje i za ovaj deo(ovo sam guglala)
     const content = document.querySelectorAll(".tab-content");
     content.forEach((c) => c.classList.remove("active"));
 
@@ -13,5 +12,45 @@ tabs.forEach(function (tab) {
     const id = name + "-section";
 
     document.getElementById(id).classList.add("active");
+  });
+});
+
+const form = document.getElementById("entry-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const dateElement = document.getElementById("date");
+  const dateValue = dateElement.value;
+  console.log(dateValue);
+
+  const hoursElement = document.getElementById("number");
+  const hoursValue = Number(hoursElement.value);
+  console.log(hoursValue);
+
+  const challengeElement = document.getElementById("text");
+  const challengeValue = challengeElement.value.trim();
+  console.log(challengeValue);
+
+  const noteElement = document.getElementById("note");
+  const noteValue = noteElement.value.trim();
+  console.log(noteValue);
+
+  const entry = {
+    date: dateValue,
+    hours: hoursValue,
+    intesity: intesity,
+    challenge: challengeValue,
+    note: noteValue,
+  };
+  console.log(entry);
+});
+
+let intesity = 0;
+const button = document.querySelectorAll(".intesity-button");
+button.forEach(function (button) {
+  button.addEventListener("click", function (element) {
+    const buttonValue = Number(element.target.textContent);
+    intesity = buttonValue;
+    console.log(intesity);
   });
 });
